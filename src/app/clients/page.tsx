e2e6b8da5b9da1fc75b776020916689519e482b4
Pierre-Maurice Merckel <pierre@merckel.dev>
2024-12-06 23:25:51 +0100

    Added clients page

---
+++ src/app/clients/page.tsx
@@ -0,0 +1,46 @@
+import ProjectStats from "@/components/StatsBar";
+import ProjectItem from "@/components/ProjectItem";
+import { SkillIcons } from "@/components/SkillIcons";
+
+export default function Clients() {
+  return (
+    <section className="mb-12 md:w-1/2 md:mx-auto md:px-0">
+      <h1 className="md:text-5xl text-3xl text-center font-bold mb-12">Clients</h1>
+      <div className="mb-24">
+        <ProjectItem
+          projectName="LogiQ IT"
+          skills={[SkillIcons.github, SkillIcons.neovim, SkillIcons.astro, SkillIcons.tailwind, SkillIcons.docker]}
+          bgImage="logiqit.webp"
+          href="https://logiqit.de" />
+      </div>
+      <ProjectStats stats={[{ name: "Visitor", value: 300000 }, { name: "User", value: 3000 }, { name: "Usage", value: 300 }]} />
+      <div className="mb-8 mt-24">
+        <ProjectItem
+          projectName="Stimberg Physio"
+          skills={[SkillIcons.github, SkillIcons.neovim, SkillIcons.svelte, SkillIcons.tailwind, SkillIcons.docker]}
+          bgImage="stimberg.webp"
+          href="https://stimberg.logiqit.de/"
+        />
+      </div>
+      <div className="mb-8">
+        <ProjectItem
+          projectName="Bündnis Recklinghausen"
+          skills={[SkillIcons.github, SkillIcons.neovim, SkillIcons.astro, SkillIcons.tailwind, SkillIcons.docker]}
+          bgImage="buendnis.webp"
+          href="https://buendnis-recklinghausen.de/"
+        />
+      </div>
+      <div className="mb-8">
+        <ProjectItem
+          projectName="Marler Bestattungen"
+          skills={[SkillIcons.github, SkillIcons.neovim, SkillIcons.svelte, SkillIcons.tailwind, SkillIcons.docker]}
+          bgImage="bestattung.webp"
+          href="https://www.marler-bestattungen.de/"
+        />
+      </div>
+
+
+
+    </section>
+  )
+}
---
+++ src/components/StatsBar.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+interface Props {
+  stats: { name: string, value: number }[]
+}
+
+export default function(props: Props) {
+  const stats = props.stats.map((stat, index) => {
+    let reformated_value = ""
+    if (stat.value > 1000000) {
+      reformated_value = (stat.value / 1000000).toString() + "M"
+    } else if (stat.value > 1000) {
+      reformated_value = (stat.value / 1000).toString() + "K"
+    } else {
+      reformated_value = stat.value.toString()
+    }
+    return (
+      <div className="mx-auto" key={index}>
+        <p className="text-2xl font-bold text-center">{reformated_value}</p>
+        <p className="text-lg text-center">{stat.name}</p>
+      </div>
+
+    )
+  })
+  return (
+    <div className="w-full h-32 bg-accent text-white grid grid-cols-3 grid-rows-1 gap-4 items-center">
+      {stats}
+    </div>
+  )
+}
